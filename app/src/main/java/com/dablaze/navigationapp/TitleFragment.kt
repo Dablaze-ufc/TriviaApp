da04ac9a310903f5608ddcd5cc130d2549975626
Dablze-ufc <chukwuwauchenna@gmail.com>
2019-12-02 15:41:44 +0100

    implemented the backstack to from the game won fragment to the tittle fragment

--- app/src/main/java/com/dablaze/navigationapp/TitleFragment.kt
+++ app/src/main/java/com/dablaze/navigationapp/TitleFragment.kt
@@ -6,9 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.dablaze.navigationapp.databinding.FragmentTitleBinding
 
 /**
@@ -22,9 +22,9 @@ class TitleFragment : Fragment() {
     ): View? {
         val binding: FragmentTitleBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener { view: View ->
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 }
